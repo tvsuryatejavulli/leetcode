c300756cd0e880fa2f9931150b673053a38eec78
tvsuryatejavulli <suryatejavulli@gmail.com>
2025-05-21 10:02:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (19.60%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        n=len(nums)
+        if n==1:
+            return nums[0]
+        if nums[n//2]==nums[0] or nums[n//2]==nums[-1] or  nums[n//2]==nums[n//2-1] or nums[n//2]==nums[n//2+1]:
+            return nums[n//2]
