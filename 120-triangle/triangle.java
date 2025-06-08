dee4a0f398e03981fcf2c6f9e650b6f157bd2e86
tvsuryatejavulli <suryatejavulli@gmail.com>
2025-06-08 07:30:14 +0530

    Time: 8 ms (13.32%) | Memory: 44.4 MB (43.49%) - LeetSync

---
+++ 120-triangle/triangle.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        
+        for (int row = n - 2; row >= 0; row--) {
+            for (int col = 0; col < triangle.get(row).size(); col++) {
+                triangle.get(row).set(col, triangle.get(row).get(col) +
+                    Math.min(triangle.get(row + 1).get(col), triangle.get(row + 1).get(col + 1)));
+            }
+        }
+        
+        return triangle.get(0).get(0);
+    }
+}
