30773da9f96b83812675df92b36c884a74bf4f55
tvsuryatejavulli <suryatejavulli@gmail.com>
2025-08-30 17:29:39 +0530

    Time: 6 ms (42.56%) | Memory: 18 MB (5.46%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,35 +1,18 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
-        def is_valid_unit(unit):
-            unit = [x for x in unit if x != '.']
-            return len(unit) == len(set(unit))
-        
-        def get_box(board, box_index):
-            box = []
-            row_start = (box_index // 3) * 3
-            col_start = (box_index % 3) * 3
-            for i in range(3):
-                for j in range(3):
-                    box.append(board[row_start + i][col_start + j])
-            return box
-        
-        # Check rows
-        for row in board:
-            if not is_valid_unit(row):
-                return False
-        
-        # Check columns
-        for col in zip(*board):
-            if not is_valid_unit(col):
-                return False
-        
-        # Check 3x3 sub-boxes
-        for i in range(9):
-            if not is_valid_unit(get_box(board, i)):
-                return False
-        
-        return True
+class Solution:
+  def isValidSudoku(self, board: list[list[str]]) -> bool:
+    seen = set()
+
+    for i in range(9):
+      for j in range(9):
+        c = board[i][j]
+        if c == '.':
+          continue
+        if (c + '@row ' + str(i) in seen or
+            c + '@col ' + str(j) in seen or
+                c + '@box ' + str(i // 3) + str(j // 3) in seen):
+          return False
+        seen.add(c + '@row ' + str(i))
+        seen.add(c + '@col ' + str(j))
+        seen.add(c + '@box ' + str(i // 3) + str(j // 3))
+
+    return True
